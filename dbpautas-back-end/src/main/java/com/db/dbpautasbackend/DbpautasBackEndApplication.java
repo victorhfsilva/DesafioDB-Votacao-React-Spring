0a350e7527ec027c7d38a3d5ab9a0a1f1a8c1e07
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-01 15:24:34 -0300

    init: Inicialização do Projeto e Configuração do Banco de Dados.

---
+++ dbpautas-back-end/src/main/java/com/db/dbpautasbackend/DbpautasBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.db.dbpautasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpautasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbpautasBackEndApplication.class, args);
+	}
+
+}
