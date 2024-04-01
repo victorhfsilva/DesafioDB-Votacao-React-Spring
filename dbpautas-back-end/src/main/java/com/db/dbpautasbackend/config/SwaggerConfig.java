725cf1a272f970cc50a1dd332f1896c609fd143c
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-01 15:55:10 -0300

    feat: Configuração do Swagger.

---
+++ dbpautas-back-end/src/main/java/com/db/dbpautasbackend/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.db.dbpautasbackend.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()
+                                .addList("Bearer Authentication"))
+                                .components(new Components().addSecuritySchemes
+                                        ("Bearer Authentication", createAPIKeyScheme()))
+                            .info(new Info().title("DBPautas - API")
+                            .description("Desafio técnico da DBServer.")
+                            .version("1.0").contact(new Contact().name("Victor Silva")
+                            .email("victor.ferreira@db.tec.br")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                                    .bearerFormat("JWT")
+                                    .scheme("bearer");
+    }
+
+
+}
