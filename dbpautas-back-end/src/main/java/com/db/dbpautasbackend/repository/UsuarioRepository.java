c99b5ef2786233549cdb60b39b7d8757dd7a440a
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-01 20:20:26 -0300

    feat: Criação do Repositório dos Usuários.

---
+++ dbpautas-back-end/src/main/java/com/db/dbpautasbackend/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.db.dbpautasbackend.repository;
+
+import com.db.dbpautasbackend.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCpf(String cpf);
+
+}
