bfa86135bc483d31f270a000578e23149d798c4f
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-06 19:47:16 -0300

    feat: Implementação do controlador de registrar usuários.

---
+++ dbpautas-back-end/src/main/java/com/db/dbpautasbackend/mapper/UsuarioMapper.java
@@ -0,0 +1,17 @@
+package com.db.dbpautasbackend.mapper;
+
+import com.db.dbpautasbackend.dto.RegistrarUsuarioDTO;
+import com.db.dbpautasbackend.model.Usuario;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public interface UsuarioMapper {
+    static Usuario mapRegistrarUsuarioDTOToUsuario(RegistrarUsuarioDTO usuarioDTO, PasswordEncoder passwordEncoder){
+        return Usuario.builder().nome(usuarioDTO.nome())
+                .sobrenome(usuarioDTO.sobrenome())
+                .email(usuarioDTO.email())
+                .cpf(usuarioDTO.cpf())
+                .senha(passwordEncoder.encode(usuarioDTO.senha()))
+                .papel(usuarioDTO.papel())
+                .build();
+    }
+}
---
+++ dbpautas-back-end/src/main/java/com/db/dbpautasbackend/validators/SenhaValidator.java
@@ -0,0 +1,42 @@
+package com.db.dbpautasbackend.validators;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.passay.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SenhaValidator implements ConstraintValidator<ValidPassword, String> {
+
+    @Override
+    public void initialize(ValidPassword constraintAnnotation) {
+        // No initialization needed for this validator
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext context) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new LengthRule(8, 30),
+                new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                new CharacterRule(EnglishCharacterData.Digit, 1),
+                new CharacterRule(EnglishCharacterData.Special, 1),
+                new IllegalSequenceRule(EnglishSequenceData.Alphabetical, 3, false),
+                new IllegalSequenceRule(EnglishSequenceData.Numerical, 3, false),
+                new WhitespaceRule()));
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        if (result.isValid()) {
+            return true;
+        }
+
+        List<String> messages = validator.getMessages(result);
+        String messageTemplate = String.join(",", messages);
+        context.buildConstraintViolationWithTemplate(messageTemplate)
+                .addConstraintViolation()
+                .disableDefaultConstraintViolation();
+
+        return false;
+    }
+}
