32649273b543ed07bd9100629ce86054effa8b2b
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-07 20:55:25 -0300

    init: Configuração do React Testing Library.

--- dbpautas-front-end/src/App.spec.tsx
+++ dbpautas-front-end/src/App.spec.tsx
@@ -1,5 +1,9 @@
-describe('Test', () => {
-    it('test', () => {
+import App from "./App"
+import { render } from '@testing-library/react'
+
+describe('App', () => {
+    it('should render the App component', () => {
+        render(<App />)
         expect(1).toBe(1)
     })
 })
