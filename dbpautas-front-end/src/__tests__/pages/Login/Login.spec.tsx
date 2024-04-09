91165632d89dd14684d9199844dfcd3168d0991b
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-09 17:03:58 -0300

    test: Teste da Funcionalidade de Login.

---
+++ dbpautas-front-end/src/__tests__/pages/Login/Login.spec.tsx
@@ -0,0 +1,43 @@
+import { fireEvent, render, waitFor } from "@testing-library/react";
+import LoginRespostaModel from "../../../models/LoginRespostaModel";
+import { ChakraProvider } from "@chakra-ui/react";
+import defaultTheme from "../../../themes/default"
+import Login from "../../../pages/Login";
+import loginService from "../../../services/login.service";
+
+const loginResposta: LoginRespostaModel = {
+    token: 'token',
+    papel: 'ADMIN'
+}
+
+jest.mock('../../../services/login.service', () => {
+    return jest.fn().mockResolvedValue(loginResposta)
+});
+
+it('deveria obter token com sucesso', async () => {
+    render(
+        <ChakraProvider theme={defaultTheme}>
+            <Login />
+        </ChakraProvider>
+    );
+
+    const cpfInput = document.querySelector('input[name="cpf"]');
+    const senhaInput = document.querySelector('input[name="senha"]');
+    const submitButton = document.querySelector('button[type="submit"]');
+
+    if (cpfInput) {
+        fireEvent.change(cpfInput, { target: { value: '22797475291' } });
+    }
+
+    if (senhaInput) {
+        fireEvent.change(senhaInput, { target: { value: 'senha123' } });
+    }
+
+    if (submitButton) {
+        fireEvent.click(submitButton);
+    }
+
+    await waitFor( () => expect(loginService).toHaveBeenCalledWith({cpf: "22797475291", senha: "senha123"}))
+    await waitFor( () => expect(localStorage.getItem('token')).toBe('token'))
+
+});
