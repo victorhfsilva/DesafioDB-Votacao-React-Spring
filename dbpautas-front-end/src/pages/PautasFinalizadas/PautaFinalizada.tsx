64dfffe75160769564b99e95ff027a24eeafbee5
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-11 19:53:07 -0300

    feat: Implementação do modal de pautas finalizadas.

--- dbpautas-front-end/src/pages/PautasFinalizadas/PautaFinalizada.tsx
+++ dbpautas-front-end/src/pages/PautasFinalizadas/PautaFinalizada.tsx
@@ -1,62 +1,76 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text, useDisclosure } from "@chakra-ui/react";
 import PautaFinalizadaRespostaModel from "../../models/PautaFinalizadaRespostaModel";
+import PautaFinalizadaModal from "./PautaFinalizadaModal";
 
 interface PautaFinalizadaProps {
     pauta: PautaFinalizadaRespostaModel;
 }
 
 const PautaFinalizada: React.FC<PautaFinalizadaProps>  = ({pauta}) => {
-    return (
-        <Flex 
-        background={'cinza1'} 
-        height={'17em'} 
-        width={'20em'} 
-        align={'flex-start'} 
-        margin={'1.6vw'} 
-        borderRadius={'1em'} 
-        direction={'column'}>
-            <Flex width={'100%'} direction={'row'} justifyContent={'space-between'}>
-                <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
-                    #{pauta.id}
-                </Text>
-                <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
-                {(() => {
-                    switch (pauta.decisao) {
-                        case 'APROVADO':
-                            return 'Aprovado';
-                        case 'REPROVADO':
-                            return 'Reprovado';
-                        case 'EMPATE':
-                            return 'Empate';
-                    }
-                })()}
-                </Text>
-            </Flex>
+    const {isOpen, onOpen, onClose} = useDisclosure();
 
-            <Heading  
-                textAlign={'left'}
-                padding={'0em 1em 0em 0.6em'} 
-                color={'cinza4'} 
-                fontSize={'1.2em'} 
-                fontFamily={'Poppins'} 
-                fontWeight={'700'}
-                maxHeight={'2.5em'}
-                overflow={'hidden'}>
-                    {pauta.titulo}
-            </Heading>
-            <Text color={'cinza4'} 
-                fontFamily={'Poppins'} 
-                padding={'0.8em 1em 0.8em 1em'}
-                overflow={'auto'}
-                maxHeight={'7em'}>
-                <b>Resumo:</b> {pauta.resumo}
-            </Text>
-            <Flex width={'100%'} direction={'row'} justifyContent={'flex-end'}>
-                <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
-                    Sim: {pauta.votosSim} | N: {pauta.votosNao}
+    return (
+        <>
+            <PautaFinalizadaModal pauta={pauta} isOpen={isOpen} onClose={onClose}/>
+            <Flex 
+            background={'cinza1'} 
+            width={'20em'} 
+            align={'flex-start'} 
+            margin={'1.6vw'} 
+            borderRadius={'1em'} 
+            direction={'column'}>
+                <Flex width={'100%'} direction={'row'} justifyContent={'space-between'}>
+                    <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
+                        #{pauta.id}
+                    </Text>
+                    <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
+                    {(() => {
+                        switch (pauta.decisao) {
+                            case 'APROVADO':
+                                return 'Aprovado';
+                            case 'REPROVADO':
+                                return 'Reprovado';
+                            case 'EMPATE':
+                                return 'Empate';
+                        }
+                    })()}
+                    </Text>
+                </Flex>
+
+                <Heading  
+                    textAlign={'left'}
+                    padding={'0em 1em 0em 0.8em'} 
+                    color={'cinza4'} 
+                    fontSize={'1.2em'} 
+                    fontFamily={'Poppins'} 
+                    fontWeight={'700'}
+                    maxHeight={'2.5em'}
+                    overflow={'hidden'}>
+                        {pauta.titulo}
+                </Heading>
+                <Text color={'cinza4'} 
+                    fontFamily={'Poppins'} 
+                    padding={'0.8em 1em 0.8em 1em'}
+                    overflow={'auto'}
+                    maxHeight={'7em'}>
+                    <b>Resumo:</b> {pauta.resumo}
                 </Text>
+                <Flex width={'100%'} direction={'row'} justifyContent={'space-between'}>
+                    <Text color={'cinza4'} fontFamily={'Poppins'} padding={'0.8em 1em 0.8em 1em'}>
+                        Sim: {pauta.votosSim} | Não: {pauta.votosNao}
+                    </Text>
+                    <Button 
+                        width={'26%'} 
+                        background={'cinza2'} 
+                        color={'cinza4'} 
+                        _hover={{background: 'cinza5', color: 'cinza4'}} 
+                        _active={{background: 'cinza5', color: 'cinza4'}}
+                        onClick={onOpen}>
+                            Sobre
+                    </Button>
+                </Flex>
             </Flex>
-        </Flex>
+        </>
     )
 }
 
--- dbpautas-front-end/src/themes/tokens/cores.ts
+++ dbpautas-front-end/src/themes/tokens/cores.ts
@@ -1,4 +1,5 @@
 const cores = {
+    cinza5: '#74747444',
     cinza4: '#424242ff',
     cinza3: '#747474ff',
     cinza2: '#E8E8E8ff',
