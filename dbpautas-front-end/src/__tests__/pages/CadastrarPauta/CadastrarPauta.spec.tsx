7fd0b47b4be574e82c98e3dc8382da9d9deae3c9
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-17 23:20:14 -0300

    refactor: Refatoração dos testes unitários do front.

--- dbpautas-front-end/src/__tests__/pages/CadastrarPauta/CadastrarPauta.spec.tsx
+++ dbpautas-front-end/src/__tests__/pages/CadastrarPauta/CadastrarPauta.spec.tsx
@@ -12,18 +12,6 @@ const cadastrarPautaRequisicao: CadastrarPautaRequisicaoModel = {
     categoria: 'FINANCAS'
 }
 
-const mockSetAutenticado = jest.fn();
-const mockSetAdmin = jest.fn();
-
-jest.mock('../../../hooks/useAuthStore', () => ({
-    __esModule: true,
-    default: () => ({
-      setAutenticado: mockSetAutenticado,
-      setAdmin: mockSetAdmin,
-    }),
-  }));
-  
-
 jest.mock('../../../services/cadastrarPauta.service', () => {
     return jest.fn().mockResolvedValue(true)
 });
@@ -35,25 +23,27 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate,
 }));
 
-it("deveria cadastrar pauta com sucesso", async () => {
-    render(
-        <ChakraProvider theme={defaultTheme}>
-            <CadastrarPauta />
-        </ChakraProvider>
-    );
-
-    const tituloInput = document.querySelector('input[name="titulo"]');
-    const resumoTextArea = document.querySelector('textarea[name="resumo"]');
-    const descricaoTextArea = document.querySelector('textarea[name="descricao"]');
-    const categoriaSelect = document.querySelector('select[name="categoria"]');
-    const submitButton = document.querySelector('button[type="submit"]');
+describe("Testes da página de cadastro de pauta", () => {
+    it("deveria cadastrar pauta com sucesso", async () => {
+        render(
+            <ChakraProvider theme={defaultTheme}>
+                <CadastrarPauta />
+            </ChakraProvider>
+        );
     
-    if (tituloInput) fireEvent.change(tituloInput, { target: { value: 'Título da Pauta' } });
-    if (resumoTextArea) fireEvent.change(resumoTextArea, { target: { value: 'Resumo da Pauta' } });
-    if (descricaoTextArea) fireEvent.change(descricaoTextArea, { target: { value: 'Descrição da Pauta' } });
-    if (categoriaSelect) fireEvent.change(categoriaSelect, { target: { value: 'FINANCAS' } });
-    if (submitButton) fireEvent.click(submitButton);
-
-    await waitFor( () => expect(cadastrarPautaService).toHaveBeenCalledWith(cadastrarPautaRequisicao, mockSetAutenticado, mockSetAdmin, mockNavigate))
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/abrirPauta'));
+        const tituloInput = document.querySelector('input[name="titulo"]');
+        const resumoTextArea = document.querySelector('textarea[name="resumo"]');
+        const descricaoTextArea = document.querySelector('textarea[name="descricao"]');
+        const categoriaSelect = document.querySelector('select[name="categoria"]');
+        const submitButton = document.querySelector('button[type="submit"]');
+        
+        if (tituloInput) fireEvent.change(tituloInput, { target: { value: 'Título da Pauta' } });
+        if (resumoTextArea) fireEvent.change(resumoTextArea, { target: { value: 'Resumo da Pauta' } });
+        if (descricaoTextArea) fireEvent.change(descricaoTextArea, { target: { value: 'Descrição da Pauta' } });
+        if (categoriaSelect) fireEvent.change(categoriaSelect, { target: { value: 'FINANCAS' } });
+        if (submitButton) fireEvent.click(submitButton);
+    
+        await waitFor( () => expect(cadastrarPautaService).toHaveBeenCalledWith(cadastrarPautaRequisicao))
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/abrirPauta'));
+    })
 })
--- dbpautas-front-end/src/__tests__/pages/Login/Login.spec.tsx
+++ dbpautas-front-end/src/__tests__/pages/Login/Login.spec.tsx
@@ -21,24 +21,27 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockNavigate,
   }));
   
+describe("Testes da página de login", () => {
 
-it('deveria obter token com sucesso', async () => {
-    render(
-        <ChakraProvider theme={defaultTheme}>
-            <Login />
-        </ChakraProvider>
-    );
-
-    const cpfInput = document.querySelector('input[name="cpf"]');
-    const senhaInput = document.querySelector('input[name="senha"]');
-    const submitButton = document.querySelector('button[type="submit"]');
-
-    if (cpfInput) fireEvent.change(cpfInput, { target: { value: '22797475291' } });
-    if (senhaInput) fireEvent.change(senhaInput, { target: { value: 'senha123' } });
-    if (submitButton) fireEvent.click(submitButton);
-
-    await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/'));
-    await waitFor( () => expect(loginService).toHaveBeenCalledWith({cpf: "22797475291", senha: "senha123"}))
-    await waitFor( () => expect(localStorage.getItem('token')).toBe('token'))
-
-});
+    it('deveria obter token com sucesso', async () => {
+        render(
+            <ChakraProvider theme={defaultTheme}>
+                <Login />
+            </ChakraProvider>
+        );
+    
+        const cpfInput = document.querySelector('input[name="cpf"]');
+        const senhaInput = document.querySelector('input[name="senha"]');
+        const submitButton = document.querySelector('button[type="submit"]');
+    
+        if (cpfInput) fireEvent.change(cpfInput, { target: { value: '22797475291' } });
+        if (senhaInput) fireEvent.change(senhaInput, { target: { value: 'senha123' } });
+        if (submitButton) fireEvent.click(submitButton);
+    
+        await waitFor(() => expect(mockNavigate).toHaveBeenCalledWith('/'));
+        await waitFor( () => expect(loginService).toHaveBeenCalledWith({cpf: "22797475291", senha: "senha123"}))
+        await waitFor( () => expect(localStorage.getItem('token')).toBe('token'))
+    
+    });
+    
+})
