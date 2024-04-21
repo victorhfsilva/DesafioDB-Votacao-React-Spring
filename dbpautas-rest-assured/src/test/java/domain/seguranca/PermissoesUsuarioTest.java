079ac7f70d0269cd18c534dbea68bf7e9308b6a9
Victor Hugo Ferreira Silva <victorhfsilva@protonmail.com>
2024-04-20 22:59:12 -0300

    test: Testes de Segurança.

---
+++ dbpautas-rest-assured/src/test/java/domain/seguranca/PermissoesUsuarioTest.java
@@ -0,0 +1,84 @@
+package domain.seguranca;
+
+import fixture.usuario.AdminFixture;
+import fixture.usuario.LoginFixture;
+import fixture.usuario.UsuarioFixture;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.example.domain.usuario.LoginRequisicao;
+import org.example.domain.usuario.UsuarioRequisicao;
+import org.junit.jupiter.api.*;
+
+public class PermissoesUsuarioTest {
+
+    public static RequestSpecification request;
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "http://localhost:8080/";
+    }
+
+    @BeforeEach
+    void setRequest() {
+
+        request = RestAssured.given().contentType("application/json; charset=UTF-8");
+
+        LoginRequisicao loginAdmin = LoginFixture.builderValido();
+        Response responseAdmin = request.body(loginAdmin).post("/login");
+        String tokenAdmin = responseAdmin.jsonPath().getString("token");
+
+        request = request.header("Authorization", "Bearer " + tokenAdmin);
+
+        UsuarioRequisicao usuario = UsuarioFixture.builderValido();
+        request.body(usuario)
+                .post("/usuario/registrar");
+
+        LoginRequisicao login = LoginFixture.builderDoUsuario(usuario);
+        Response response = request.body(login).post("/login");
+        String token = response.jsonPath().getString("token");
+
+        request = RestAssured.given().contentType("application/json; charset=UTF-8");
+        request = request.header("Authorization", "Bearer " + token);
+    }
+
+    @Test
+    @DisplayName("Dado um usuário comum, quando o usuário tenta cadastrar um usuário, então o sistema deve retornar status 403.")
+    void deveRetornarStatus403QuandoUsuarioTentaCadastrarAdmin(){
+        UsuarioRequisicao admin = AdminFixture.builderValido();
+        request.body(admin)
+                .post("/usuario/registrar")
+                .then().statusCode(403);
+    }
+
+    @Test
+    @DisplayName("Dado um usuário comum, quando o usuário tentar registrar uma pauta, então o sistema deve retornar status 403.")
+    void deveRetornarStatus403QuandoUsuarioTentaRegistrarPauta(){
+        UsuarioRequisicao usuario = UsuarioFixture.builderValido();
+        request.body(usuario)
+                .post("/usuario/registrar")
+                .then().statusCode(403);
+    }
+
+    @Test
+    @DisplayName("Dado um usuário comun, quando o usuário tenta obter as pautas abertas, então o sistema deve retornar com status 200.")
+    void deveRetornarStatus403QuandoUsuarioTentaObterPautasAbertas(){
+        request.get("/pauta/aberta/")
+                .then().statusCode(200);
+    }
+
+    @Test
+    @DisplayName("Dado um usuário comun, quando o usuário tenta obter as pautas fechadas, então o sistema deve retornar com status 200.")
+    void deveRetornarStatus403QuandoUsuarioTentaObterPautasFechadas(){
+        request.get("/pauta/fechada/")
+                .then().statusCode(403);
+    }
+
+    @Test
+    @DisplayName("Dado um usuário comun, quando o usuário tenta obter as pautas finalizadas, então o sistema deve retornar com status 200.")
+    void deveRetornarStatus403QuandoUsuarioTentaObterPautasFinalizadas(){
+        request.get("/pauta/finalizada/")
+                .then().statusCode(200);
+    }
+
+}
--- dbpautas-rest-assured/src/test/java/fixture/usuario/LoginFixture.java
+++ dbpautas-rest-assured/src/test/java/fixture/usuario/LoginFixture.java
@@ -1,9 +1,13 @@
 package fixture.usuario;
 
 import org.example.domain.usuario.LoginRequisicao;
+import org.example.domain.usuario.UsuarioRequisicao;
 
 public interface LoginFixture {
 
+    static LoginRequisicao builderDoUsuario(UsuarioRequisicao usuario) {
+        return builder().cpf(usuario.getCpf()).senha(usuario.getSenha()).build();
+    }
     static LoginRequisicao builderValido() {
         return builder().build();
     }
